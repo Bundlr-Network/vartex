2683e5467c77e9b93f84b8b0d378572ed7bd0f6e
Josh Benaron <joshbenaron@gmail.com>
2022-06-11 23:46:50 +0100

    Test

--- cli/_import-tx.ts
+++ cli/_import-tx.ts
@@ -3,7 +3,7 @@ import { config } from "dotenv";
 
 config();
 
-console.log(process.env.ARWEAVE_NODES);
+console.log(typeof process.env.ARWEAVE_NODES);
 
 // txId, blockHeight
 const args = process.argv.slice(2);
