8a9176e3de0911514f86ce2d16896e89e6438339
Josh Benaron <joshbenaron@gmail.com>
2022-06-11 23:31:51 +0100

    Test

--- src/queue/config.ts
+++ src/queue/config.ts
@@ -1,7 +1,7 @@
 export const REDIS_CONFIG = {
     redis: {
-        host: process.env.REDIS_HOST,
-        port: +process.env.REDIS_PORT,
+        host: process.env.REDIS_HOST ?? "localhost",
+        port: +(process.env.REDIS_PORT ?? 6379),
         username: process.env.REDIS_USER,
         password: process.env.REDIS_PASSWORD,
     },
--- src/queue/index.ts
+++ src/queue/index.ts
@@ -6,7 +6,8 @@ import { REDIS_CONFIG } from "./config";
 export type ImportTxJob = { tx_id: string, block_hash: string, block_height: number };
 export type ImportBlockJob = { bundleId: number, txId: string } | { bundleId: number };
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export type S3Job = { txId: string, tx?: any };
+export type ImportBundleJob = { tx_id: string };
 
 export const importTxQueue = new MQ.Queue<ImportTxJob>("Import Tx Queue", { connection: REDIS_CONFIG.redis });
+export const importBundleQueue = new MQ.Queue<ImportBundleJob>("Import Bundle Queue", { connection: REDIS_CONFIG.redis });
 
