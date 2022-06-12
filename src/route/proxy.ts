9ba71f8dbf429b3f817821bcb86bb214824b7cb0
Josh Benaron <joshbenaron@gmail.com>
2022-06-12 21:30:03 +0100

    Progress

--- src/route/proxy.ts
+++ src/route/proxy.ts
@@ -18,23 +18,29 @@ export function proxyGetRoute(request: Request, response: Response): void {
   stream.pipe(response);
 }
 
-export function proxyPostRoute(request: Request, response: Response): void {
+export async function proxyPostRoute(request: Request, response: Response): void {
   const uri = `${grabNode()}${request.originalUrl}`;
-  const stream = undici.fetch(uri, {
-    method: "POST",
-    body: request,
-    headers: request.headers
-  });
-  stream.on("error", (error) => {
-    console.log(error);
+  try {
+    const stream = await undici.request(uri, {
+      method: "POST",
+      body: request,
+      headers: request.headers
+    });
 
-    response.status(503).send().end();
-    console.log(`[POST] Failed to post: ${uri}`);
-  });
-  stream.on("end", () => response.end());
-  stream.on("error", (e) => {
-    console.error(`Error occurred while piping chunks to ${uri} - ${e}`);
-    response.status(500).end();
-  })
-  stream.pipe(response);
+    stream.body.pipe(response);
+  } catch (error) {
+    console.error(`Error occurred while piping chunks to ${uri} - ${error}`);
+  }
+
+  // stream.body.on("error", (error) => {
+  //   console.log(error);
+  //
+  //   response.status(503).send().end();
+  //   console.log(`[POST] Failed to post: ${uri}`);
+  // });
+  // stream.on("end", () => response.end());
+  // stream.on("error", (e) => {
+  //   console.error(`Error occurred while piping chunks to ${uri} - ${e}`);
+  //   response.status(500).end();
+  // })
 }
