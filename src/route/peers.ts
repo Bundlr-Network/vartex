a65d5c5fa8d465fcdfc659141a57ecd8fc42c0bf
Josh Benaron <joshbenaron@gmail.com>
2022-06-14 09:41:35 +0100

    Progress

--- src/route/peers.ts
+++ src/route/peers.ts
@@ -7,5 +7,5 @@ export async function peers(request: Request, response: Response): Promise<void>
         .then(r => r.body.json());
 
     response.contentType("application/json");
-    response.send([...JSON.parse(process.env.ARWEAVE_NODES), ...peers]);
+    response.send([...JSON.parse(process.env.ARWEAVE_NODES).map((r: string) => r.slice(r.indexOf("//") + 1)), ...peers]);
 }
