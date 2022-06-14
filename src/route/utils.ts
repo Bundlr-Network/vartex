0da32f38814a05651e3f2421c8e51056849bae00
Josh Benaron <joshbenaron@gmail.com>
2022-06-14 13:00:03 +0100

    Progress

--- src/route/utils.ts
+++ src/route/utils.ts
@@ -1,7 +1,7 @@
 import { Response } from "express";
 
 export function makeError(response: Response, code: number, message?: string): Promise<void> {
-    response.status(code);
+    response.sendStatus(code);
     if (message) response.send(message);
     response.end();
     return;
