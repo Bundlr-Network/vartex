c7b51d1ff04308dceffac1baf385693e5138dcee
Josh Benaron <joshbenaron@gmail.com>
2022-06-14 14:28:00 +0100

    Progress

--- scripts/addBundle.ts
+++ scripts/addBundle.ts
@@ -1,6 +1,7 @@
 import { importBundleQueue } from "../src/queue";
 
 importBundleQueue.add("Import Bundle", {
-    tx_id: process.argv[2],
+    txId: process.argv[2],
+    blockHeight: 100,
     type: "ANS104"
 })
